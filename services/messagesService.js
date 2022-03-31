628394392ef1c33d0d48baf6ee344f90db5eae5a
Arvin Jayson Tamayo Castro <arvin.castro@renewsoftware.co>
2022-03-31 16:13:14 +0800

    ToLowerCase, Context

--- services/messagesService.js
+++ services/messagesService.js
@@ -1,19 +1,24 @@
 // internal database (code) can be on database, or object, yet out of scope
 
 var internalDatabase = [
-  {context: ["Hello", "Hi"], response: "Welcome to StationFive."},
-  {context: ["Goodbye", "bye"], response: "Thank you, see you around."}
+  { contextLowerCase: ["hello", "hi"], context: ["Hello", "Hi"], response: "Welcome to StationFive." },
+  { contextLowerCase: ["goodbye", "bye"], context: ["Goodbye", "bye"], response: "Thank you, see you around." }
 ]
 
 exports.post = function (message) {
-  var element = internalDatabase.find(data => {
-    var isExists = false;
+  var messageLowerCase = message.toLowerCase();
+  var index = -1;
+  var response = "";
+  var contextFound = ""; // note, this can be used in the future, for now, just set its value
+  internalDatabase.foreach (data => {
     data.context.forEach(context => {
-      if(message.includes(context)) {
-        isExists = true; 
+      var indexOfContext = messageLowerCase.indexOf(context.toLowerCase);
+      if (indexOfContext != -1 && (index === -1 || index > indexOfContext)) { // note, decided to have internalDatabase.contextLowerCase for faster output (this in concept should have input of .toLowerCase() when inputing)
+        index = indexOfContent;
+        contextFound = context;
+        response = data.response;
       }
     });
-    return isExists;
   });
-  return element ? element.response : "Sorry, I don't understand."
+  return contextFound ? response : "Sorry, I don't understand."
 }
